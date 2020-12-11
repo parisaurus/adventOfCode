ac07f9950b03fc1fecf2a0e62546608507991eb1
Dani Pariente <anubisk95@gmail.com>
2020-12-11 12:26:27 +0100

    day2part1

---
+++ day2/advent2.py
@@ -0,0 +1,18 @@
+file = open("input.txt", "r")
+
+def letterCount(letter, word, minMax):
+    list_of_letters = list(word)
+    count = 0
+    for x in list_of_letters:
+        if letter[:-1] == x:
+            count += 1
+    return count >= int(minMax[0]) and count <= int(minMax[1])
+
+lista =file.read().split("\n")
+contador = 0
+for x in lista[:-1]:
+    aux = x.split(" ")
+    minMax = aux[0].split("-")
+    if letterCount(aux[1], aux[2], minMax):
+        contador += 1
+print(contador)

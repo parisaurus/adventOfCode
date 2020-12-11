7ab64b453324d67f435a5a5922fcd6771f33e7ab
Dani Pariente <anubisk95@gmail.com>
2020-12-11 12:49:47 +0100

    remove redundant

--- day2/advent2part1.py
+++ day2/advent2part1.py
@@ -18,20 +18,3 @@ for x in lista[:-1]:
     if letterCount(aux[1], aux[2], minMax):
         contador += 1
 print(contador)
-
-#part2
-
-def letterCount2(letter, word, minMax):
-    list_of_letters = list(word)
-    print(letter[:-1])
-    
-    return ((letter[:-1] == list_of_letters[minMax[0]]) ^ (letter[:-1] == list_of_letters[minMax[1]]))
-
-lista =file.read().split("\n")
-contador = 0
-for x in lista[:-1]:
-    aux = x.split(" ")
-    minMax = aux[0].split("-")
-    if letterCount2(aux[1], aux[2], minMax):
-        contador += 1
-print(contador)

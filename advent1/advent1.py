c68fb7426d1ed15c2f28da7b31911b4cb58d3800
Dani Pariente <anubisk95@gmail.com>
2020-12-11 11:48:54 +0100

    Day1part2

--- advent1/advent1.py
+++ advent1/advent1.py
@@ -3,7 +3,15 @@ file = open("input.txt", "r")
 #print(file.read())
 lista = [int(x) for x in file.read().split("\n")]
 
+#part1
 for x in range(len(lista)):
     for j in range(x,len(lista)):
         if(lista[x] + lista[j]) == 2020:
             print(lista[x] * lista[j])
+
+#part2
+for x in range(len(lista)):
+    for j in range(x,len(lista)):
+        for k in range(j,len(lista)):
+            if(lista[x] + lista[j] + lista[k]) == 2020:
+                print(lista[x] * lista[j] * lista[k])

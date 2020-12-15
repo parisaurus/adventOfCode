6151ca376a5b0d837fd3996ce7ed5251358ec0dd
Dani Pariente <anubisk95@gmail.com>
2020-12-15 12:49:55 +0100

    day4 part 1

---
+++ day4/advent4part1.py
@@ -0,0 +1,24 @@
+file = open("input.txt", "r")
+text = file.read().split("\n\n")
+
+def validate(text, i):
+    necessaryFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+
+    for items in necessaryFields: 
+        if items not in text[i]:
+            print("Item ", i, " missing ", items)
+            return True
+
+#number of items in the text
+aux = 0
+for items in text:
+    aux = aux + 1
+
+#bucle
+for i, item in enumerate(text):
+    if validate(text, i):
+        aux = aux - 1
+
+
+print("Number correct passports = ", aux )
+
